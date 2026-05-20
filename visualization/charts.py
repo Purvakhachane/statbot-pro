0dbbc88cd5c34a7b9511ac756e707e93d418ae94
Purvakhachane <purvaykhachane05@gmail.com>
2026-05-20 21:23:05 +0530

    Added visualization module

---
+++ visualization/charts.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def create_bar_chart(df, x_col, y_col):
+    plt.figure(figsize=(8,5))
+    plt.bar(df[x_col], df[y_col])
+
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+    plt.title(f"{y_col} by {x_col}")
+
+    plt.show()
