0f85a825f9c83937f887386e842cc662669c15b1
Purvakhachane <purvaykhachane05@gmail.com>
2026-05-20 21:33:59 +0530

    Add labels and title to bar chart

--- visualization/charts.py
+++ visualization/charts.py
@@ -5,6 +5,7 @@ def create_bar_chart(df, x_col, y_col):
     plt.figure(figsize=(8,5))
     plt.bar(df[x_col], df[y_col])
 
+
     plt.xlabel(x_col)
     plt.ylabel(y_col)
     plt.title(f"{y_col} by {x_col}")
