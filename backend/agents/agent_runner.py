e9f738ff4e62d8f03e6ec6f104395a50e23b811c
Humera Sache <humerasache@gmail.com>
2026-06-06 21:17:44 +0530

    Added automatic self correction and retry logic

--- backend/agents/agent_runner.py
+++ backend/agents/agent_runner.py
@@ -1,16 +1,40 @@
 from backend.agents.query_interpreter import interpret_query
 from backend.agents.pandas_agent import execute_query
+from backend.agents.self_corrector import correct_query
+
+MAX_RETRIES = 3
 
 def run_agent(df, user_query):
     try:
         parsed_query = interpret_query(user_query)
-        result = execute_query( df, parsed_query)
+        attempt = 0
+        while attempt < MAX_RETRIES:
+            result = execute_query(df, parsed_query)
+
+            if not (
+                isinstance(result, dict)
+                and result.get("status") == "error"
+            ):
+
+                return {
+                    "status": "success",
+                    "query": user_query,
+                    "parsed_query": parsed_query,
+                    "result": result
+                }
+
+            parsed_query = correct_query(
+                user_query=user_query,
+                previous_query=parsed_query,
+                error_message=result.get("message"),
+                available_columns=list(df.columns)
+            )
+
+            attempt += 1
 
         return {
-            "status": "success",
-            "query": user_query,
-            "parsed_query": parsed_query,
-            "result": result
+            "status": "error",
+            "message": "Maximum retry limit reached."
         }
 
     except Exception as error:
