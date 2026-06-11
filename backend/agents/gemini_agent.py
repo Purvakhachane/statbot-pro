f4da58308364a0b3399b9a7565a1864d97d9443b
praveen161622 <praveenvenkat1919@gmail.com>
2026-06-11 20:22:16 +0530

    Added Gemini AI assistant integration and dashboard improvements

--- backend/agents/gemini_agent.py
+++ backend/agents/gemini_agent.py
@@ -1,16 +1,18 @@
 import os
 
 from dotenv import load_dotenv
+load_dotenv()
+
+print("GEMINI_API_KEY =", os.getenv("GEMINI_API_KEY"))
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-load_dotenv()
 
 def get_gemini_model():
     """
     Creates and returns Gemini model instance
     """
     model = ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
+        model="gemini-2.5-flash",
         google_api_key=os.getenv("GEMINI_API_KEY"),
         temperature=0
     )
